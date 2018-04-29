7fe99eeaa01beeafaa7bc2fce991360393c1130d
Mirco Bertelli <mircobe87@gmail.com>
2018-04-29 19:06:24 +0200

    definiti tipi Point e Line ed esportati i cotruttori

--- js/code-art.js
+++ js/code-art.js
@@ -14,43 +14,76 @@
 
     };
 
-    function getPointList(centerPoint, radius, pointAmount) {
+    function _getPointList(centerPoint, radius, pointAmount) {
         let points = [];
         for (let i=0; i<pointAmount; i++) {
             let alpha = (i/pointAmount) * 2 * Math.PI
-            points.push({
-                x: centerPoint.x + radius*Math.sin(alpha),
-                y: centerPoint.y + radius*Math.cos(alpha)
-            });
+            points.push(new Point(
+                centerPoint.x + radius*Math.sin(alpha),
+                centerPoint.y + radius*Math.cos(alpha)
+            ));
         }
         return points;
     }
 
-    function getPerpendicularLineOnPoint(srcLine, point) {
+    function _getPerpendicularLineOnPoint(srcLine, point) {
         let m = -1 / srcLine.m;
-        return {
-            m: m,
-            q: point.y - m*point.x
+        return new Line(m, point.y - m*point.x);
+    }
+
+    function _getDistance(point0, point1) {
+        return Math.sqrt(Math.pow(point1.x - point0.x, 2) + Math.pow(point1.y - point0.y, 2));
+    }
+
+    function _getLineByPoints(point0, point1) {
+        return new Line(
+            (point0.y - point1.y) / (point0.x - point1.x),
+            (point1.y*point0.x - point0.y*point1.x) / (point0.x - point1.x)
+        );
+    }
+
+    function _getIntersectionPoint(line0, line1) {
+        return new Point(
+            (line1.q - line0.q) / (line0.m - line1.m),
+            (line0.m * line1.q - line1.m * line0.q) / (line0.m - line1.m)
+        );
+    }
+
+    function Point(x,y) {
+        this.x = x;
+        this.y = y;
+
+        this.toString = function() {
+            return "(" + this.x + ", " + this.y + ")";
+        };
+
+        this.distanceTo = function(point) {
+            return _getDistance(this, point);
         };
     }
 
-    function getDistance(point0, point1) {
-        return Math.sqrt(Math.pow(point1.x - point0.x, 2) - Math.pow(point1.y - point0.y, 2));
-    }
+    function Line(m, q) {
+        this.m = m;
+        this.q = q;
 
-    function getLineByPoints(point0, point1) {
-        return {
-            m: (point0.y - point1.y) / (point0.x - point1.x),
-            q: (point1.y*point0.x - point0.y*point1.x) / (point0.x - point1.x)
+        this.toString = function() {
+            return "y = " + this.m + "x + " + this.q;
+        };
+
+        this.perpendicularLineAt = function(point) {
+            let line = _getPerpendicularLineOnPoint(this, point);
+            return new Line(line.m, line.q);
         };
     }
 
-    function getIntersectionPoint(line0, line1) {
-        return {
-            x: (line1.q - line0.q) / (line0.m - line1.m),
-            y: (line0.m * line1.q - line1.m * line0.q) / (line0.m - line1.m)
-        };
-    }
+    CODE_ART.Geometry = {
+        Point: Point,
+        Line: Line,
+        getPerpendicularLineOnPoint: _getPerpendicularLineOnPoint,
+        getDistance: _getDistance,
+        getLineByPoints: _getLineByPoints,
+        getIntersectionPoint: _getIntersectionPoint
+    };
 
     window.CodeArt = CODE_ART;
 })();
